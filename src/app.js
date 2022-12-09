f3c144f2676c74b275159abf0d7ed1c1b127fd18
Daniel Jimenez <daniel.jimenez.cr@gmail.com>
2022-12-09 18:05:48 +0000

    add files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net"];
+
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let l in domain) {
+          console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
 };
